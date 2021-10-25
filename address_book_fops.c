c2cc69dce465adf0667108e30d5c55806990cea0
keonjoongkim <89455682+keonjoongkim@users.noreply.github.com>
2021-10-25 10:24:58 +0500

    Load function committed

--- address_book_fops.c
+++ address_book_fops.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//#include <unistd.h> This is commented out per proffesor's instructions on Teams
+#include <unistd.h>
 #include <sys/stat.h>
 #include <errno.h>
 #include <ctype.h>
@@ -10,22 +10,51 @@
 
 Status load_file(AddressBook *address_book)
 {
-	int ret;
-
-	/* 
-	 * Check for file existance
-	 */
+	int ret = access( DEFAULT_FILE, F_OK);
 
 	if (ret == 0)
 	{
-		/* 
-		 * Do the neccessary step to open the file
-		 * Do error handling
-		 */ 
+		address_book->fp = fopen(DEFAULT_FILE, "r");
+		address_book->list = (malloc) (sizeof(ContactInfo) * 30);
+		address_book->count = 0;
+    if (address_book->fp == NULL)
+    {
+      return e_fail;
+    }
+    const char s[2] = ",";
+    char line[500];
+    char *token;
+    int i;
+    while(fgets(line, sizeof line, address_book->fp) != NULL)
+    {
+        token = strtok(line, s);
+        ContactInfo c;
+        strcpy(c.name[0], token);
+        for(i=0;i<5;i++)
+        {
+          token = strtok(NULL,s);
+          strcpy(c.phone_numbers[i], token);    
+        }
+        for(i=0;i<5;i++)
+        {
+          token = strtok(NULL,s);
+          strcpy(c.email_addresses[i], token);    
+        }
+        token = strtok(NULL,s);
+        c.si_no = atoi(token);
+		address_book->list[address_book->count] = c;
+		address_book->count += 1;
+    }
+    fclose(address_book->fp);
 	}
 	else
 	{
-		/* Create a file for adding entries */
+		address_book->fp = fopen(DEFAULT_FILE, "w");
+
+		if (address_book->fp == NULL)
+		{
+		return e_fail;
+		}
 	}
 
 	return e_success;
